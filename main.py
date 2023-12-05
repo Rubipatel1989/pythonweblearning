5c7bb6c44ccbbb917d1c8a0317c9a8e3cd9aa967
Pawan Kumar <pawankum0010@gmail.com>
2023-12-05 08:05:37 +0530

    Contact Form Save

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://python_blog:python_blog@localhost/python_blog'
+
+db = SQLAlchemy(app)
+
+class Contacts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=False, nullable=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    mobile = db.Column(db.String(13), unique=True, nullable=False)
+    message = db.Column(db.String(255), unique=True, nullable=False)
+    added = db.Column(db.String(20), unique=True, nullable=False)
 
 @app.route('/')
 def home():
@@ -10,8 +23,18 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact', methods = ['GET', 'POST'])
 def contact():
+    if(request.method == 'POST'):
+        ''' Add And Entry To Database'''
+        name = request.form.get('name')   
+        email = request.form.get('email')
+        mobile = request.form.get('mobile')
+        message = request.form.get('message')
+        entry = Contacts(name = name, email = email, mobile = mobile, message = message)
+        db.session.add(entry)
+        db.session.commit()
+
     return render_template('contact.html')
 
 @app.route('/post')
